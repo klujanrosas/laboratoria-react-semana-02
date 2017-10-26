01412c207dd17ca9dbc1e99bc127f815f784c3c0
klujanrosas <elgenio.03@gmail.com>
2017-10-26 16:25:39 -0500

    proptypes example

--- app/index.js
+++ app/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
+import PropTypes from 'prop-types'
 
 import './styles/base.css'
 
-const App = () => (
+const App = ({ title, content }) => (
   <div className="title">
-    EC React - Semana 2: React Components
+    {title}: {content}
   </div>
 )
 
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired
+}
+
 render(
-  <App />,
+  <App
+    title="EC React - Semana 2"
+    content="React Components"
+  />,
   document.getElementById('root')
 )
